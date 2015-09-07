3d76880cecde4fe66afb0d3f31d556eeb2d090ea
Sonja Krause-Harder <skh@rcane.de>
2015-09-07 18:51:07 +0200

    Set up and clean up build directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,10 @@
+/* Gruntfile.js
+ * 
+ * I have left the project structure untouched. The result of the automated
+ * builds will end up in build/, the deployment tasks will pick it up from
+ * there.
+ *
+ */
 module.exports = function (grunt) {
   grunt.initConfig({
     jshint: {
@@ -7,11 +14,37 @@ module.exports = function (grunt) {
           jshintrc: true
         }
       }
+    },
+    build: {
+      options: {
+        destDir: 'build'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.registerTask('default', 'jshint');
+  grunt.registerTask('lint', 'Run lint tools over all code files', 'jshint');
+
+  grunt.registerTask('createDestDir', 
+                     'Create destination folder for build results',
+                     function () {
+    grunt.config.requires('build.options.destDir');
+    grunt.log.writeln("Creating destDir: " + 
+                      grunt.config.get("build.options.destDir"));
+    grunt.file.mkdir(grunt.config.get('build.options.destDir')); 
+  });
+
+  grunt.registerTask('deleteDestDir',
+                     'Delete destination folder for build results',
+                     function () {
+    grunt.config.requires('build.options.destDir');
+    grunt.log.writeln("Deleting destDir: " +
+                      grunt.config.get("build.options.destDir"));
+    grunt.file.delete(grunt.config.get('build.options.destDir'));
+  });
+
+  grunt.registerTask('build', 'build everything', 'createDestDir');
+  grunt.registerTask('clean', 'throw away all build results', 'deleteDestDir');
 };
 
 
